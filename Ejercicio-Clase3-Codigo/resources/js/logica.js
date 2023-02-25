a4f76301a23961b5e169c9abb76c3d42ec8beea5
granahil <laly.granadosm@gmail.com>
2023-02-25 02:57:42 -0600

    Tarea-Semana4

--- Ejercicio-Clase3-Codigo/resources/js/logica.js
+++ Ejercicio-Clase3-Codigo/resources/js/logica.js
@@ -1,2 +1,29 @@
 // declaración, es guardar o reservar un pequeño espacio en la memoria temporal//
 //var indica una variable global//
+
+var title;
+// indica de variable estructural//
+let subtitle
+//constante en un espacio de memoria que una vez asignado no va a cambiar su valor//
+const height= 200;
+
+//Inicializar es dar valor a un espacio reservado de memoria//
+title = 'Este es mi primer ejercicio de js'
+subtitle = 'que emoción!'
+
+//JS es un lengauje no tipado
+//Diferentes tipos de datos
+//string= texto
+//nuber= números 
+//array= listas
+//boolean= verdadero o falso
+
+//array
+List= ['manzanas', 'peras', 'sandía']
+
+//Objeto es la representación de un elemento del mundo real
+var user = {
+    name: 'Olman',
+    lastname: 'Santamaría'
+
+}
